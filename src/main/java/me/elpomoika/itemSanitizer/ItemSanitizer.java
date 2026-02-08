3cb4d5ff3e913e1a47b53985bf1d6663a057870e
elpomoika1 <zajcevv445@gmail.com>
2026-02-08 21:09:45 +0300

    initial commit

---
+++ src/main/java/me/elpomoika/itemSanitizer/ItemSanitizer.java
@@ -0,0 +1,12 @@
+package me.elpomoika.itemSanitizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemSanitizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
